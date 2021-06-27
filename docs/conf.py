845913045638c86212a1090729519e13ab47bfa4
lukapecnik <lukapecnik96@gmail.com>
2021-06-27 15:38:44 +0200

    Update version

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = "2021, Luka Pečnik"
 author = "Luka Pečnik"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.1"
+release = "0.1.2"
 
 
 # -- General configuration ---------------------------------------------------
--- niaclass/__init__.py
+++ niaclass/__init__.py
@@ -2,4 +2,4 @@ from niaclass.niaclass import NiaClass
 
 __all__ = ["NiaClass"]
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
