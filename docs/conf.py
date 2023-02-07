77288147d53bdd60d15d75b7d829604336ad7f57
firefly-cpp <iztok@iztok-jr-fister.eu>
2023-02-07 10:16:36 +0100

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = "2021-2022, Luka Pečnik"
 author = "Luka Pečnik"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.3"
+release = "0.1.4"
 
 
 # -- General configuration ---------------------------------------------------
