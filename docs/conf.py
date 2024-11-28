81b2e6b2d67b5b7386c1be0480b6c1d04940419d
Iztok Fister Jr. <iztok@iztok.dev>
2024-11-28 09:53:04 +0100

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = "2021-2024, Luka Pečnik"
 author = "Luka Pečnik"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.2.1"
 
 
 # -- General configuration ---------------------------------------------------
